408770962c530a416d38706623116bebc359e9ca
James Fielder <jamesfielder@jamess-macbook-pro.local>
2018-05-19 18:58:43 -0500

    enemy and sounds

--- ColorGame/GameScene.swift
+++ ColorGame/GameScene.swift
@@ -9,27 +9,117 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+
+
+class GameScene: SKScene,SKPhysicsContactDelegate {
     
     var tracksArray: [SKSpriteNode]? = [SKSpriteNode]()
+    var player:SKSpriteNode?
+    var target:SKSpriteNode?
     
-    func setupTracks(){
-        for i in 0 ... 8{
-            if let track = self.childNode(withName: "\(i)") as? SKSpriteNode{
-                tracksArray?.append(track)
+    var currentTrack = 0
+    var movingToTrack = false
+    
+    //MARK: Sound
+    
+    let moveSound = SKAction.playSoundFileNamed("move.wav", waitForCompletion: false)
+    var backgroundNoise:SKAudioNode!
+    
+    //MARK: Arrays
+    let trackVelocities = [180,200,250]
+    var directionArray = [Bool]()
+    var velocityArray  = [Int]()
+    
+    let playerCategory:UInt32 = 0x1 << 0
+    let enemyCategory:UInt32 = 0x1 << 1
+    let targetCategory:UInt32 = 0x1 << 2
+    
+   
+    override func didMove(to view: SKView) {
+        setupTracks()
+        createPlayer()
+        createTarget()
+        
+        self.physicsWorld.contactDelegate = self
+        
+        if let musicURL = Bundle.main.url(forResource: "background", withExtension: "wav"){
+            backgroundNoise = SKAudioNode(url: musicURL)
+            addChild(backgroundNoise)
+        }
+        
+        if let numberOfTracks = tracksArray?.count {
+            for _ in 0 ... numberOfTracks{
+                let randomNumberForVelocity = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+                velocityArray.append(trackVelocities[randomNumberForVelocity])
+                directionArray.append(GKRandomSource.sharedRandom().nextBool())
             }
-            //Test
+        }
+        
+        self.run(SKAction.repeatForever(SKAction.sequence([SKAction.run({
+            self.spawnEnemies()
+        }), SKAction.wait(forDuration: 2)]
+        )))
+    }
+    
+
+    
+  
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first{
+            let location = touch.previousLocation(in: self)
+            let node = self.nodes(at: location).first
+            
+            if node?.name == "right"{
+                moveToNextTrack()
+            }else if node?.name == "up"{
+                moveVertically(up: true)
+            }else if node?.name == "down"{
+                moveVertically(up: false)
+            }
+            
         }
     }
     
-    override func didMove(to view: SKView) {
-        setupTracks()
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        //tracksArray?.first?.color = UIColor.green
-       
+        if !movingToTrack{
+            player?.removeAllActions()
+        }
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        player?.removeAllActions()
     }
 
+    func didBegin(_ contact: SKPhysicsContact) {
+        var playerBody:SKPhysicsBody
+        var otherBody:SKPhysicsBody
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask{
+            playerBody = contact.bodyA
+            otherBody = contact.bodyB
+        }else{
+            playerBody = contact.bodyB
+            otherBody = contact.bodyA
+        }
+        
+        if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask == enemyCategory{
+            self.run(SKAction.playSoundFileNamed("fail.wav", waitForCompletion: true))
+            movePlayerToStart()
+        }
+        else if playerBody.categoryBitMask == playerCategory && otherBody.categoryBitMask == targetCategory{
+            nextLevel(playerPhysicsBody: playerBody)
+        }
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        if let player  = self.player {
+            if player.position.y >  self.size.height  || player.position.y < 0{
+                movePlayerToStart()
+            }
+        }
     }
 }
